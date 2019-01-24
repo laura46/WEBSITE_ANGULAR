efdaa4ccf12683867f01310d34ed3b3808c34ad0
Laura Poortenaar <wabisabicode@gmail.com>
2019-01-24 12:37:32 +0100

    commit

--- src/app/productspage/productspage.component.ts
+++ src/app/productspage/productspage.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductspageComponent implements OnInit {
 
+  public products = [
+    {'title': 'Aloë Vera', 'img': '../../assets/img/rsz_ember-ivory-431242-unsplash.jpg'},
+    {'title': 'Coconut', 'img': '../../assets/img/rsz_gerson-repreza-436184-unsplash.jpg'},
+    {'title': 'Hibiscus', 'img': '../../assets/img/rsz_jeffery-wong-539530-unsplash.jpg'},
+    {'title': 'Olive', 'img': '../../assets/img/rsz_roberta-sorge-142255-unsplash.jpg'},
+    
+  ];
   constructor() { }
 
   ngOnInit() {
