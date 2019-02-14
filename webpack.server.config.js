e500431ca6f9498fc14e2f7582223311e497bd7d
Laura <wabisabicode@gmail.com>
2019-02-14 14:56:47 +0100

    seo

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  mode: 'development',
   entry: {  server: './server.ts' },
   resolve: { extensions: ['.js', '.ts'] },
   target: 'node',
